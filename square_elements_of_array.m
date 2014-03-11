7074926975a8fdde6ceed435fcde6a81fb87799e
Per Zetterberg <per.zetterberg@ee.kth.se>
2014-03-11 13:50:09 +0100

    added square_elements ...

---
+++ square_elements_of_array.m
@@ -0,0 +1,8 @@
+function [ output_vector ] = square_elements_of_array( input_vector )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+output_vector=input_vector.^2;
+
+end
+
