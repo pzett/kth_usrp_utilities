e2418e0f1292579123aaf5db3460f0320b61da3b
perz2 <perz@ee.kth.se>
2014-03-12 10:37:26 +0100

    Added comment in square_elements_of_array.m

--- square_elements_of_array.m
+++ square_elements_of_array.m
@@ -1,6 +1,6 @@
 function [ output_vector ] = square_elements_of_array( input_vector )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% 
+%  This function squares the elements of its input vector.
 
 output_vector=input_vector.^2;
 
