5dd0c25e190608725307f2a7644c9182a5c205fc
pzett <per.zetterberg@ee.kth.se>
2014-03-28 13:39:42 +0100

    bug fix in rx_bachelor.m

--- rx_bachelor.m
+++ rx_bachelor.m
@@ -14,8 +14,8 @@ RF_freq=1784.9e6; % Center frequency.
 ref_clk=0;
 pps_trigger=0;
 gain=200;
-rx_rate=1e6;
+rx_rate=1e5;
 LOoffset=10e6;
 
-X=rx(Nsamples+20000,RF_freq,ref_clk,pps_trigger,gain,rx_rate,LOoffset);
+X=rx(Nsamples+20000,RF_freq,ref_clk,gain,rx_rate,LOoffset);
 X=X(20001:(20000+Nsamples));
