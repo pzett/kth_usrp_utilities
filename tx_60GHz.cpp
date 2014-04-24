d918c7e0b80970588d4acf37d684206d46d077b2
pzett <per.zetterberg@ee.kth.se>
2014-04-24 15:13:10 +0200

    First working experiment

--- tx_60GHz.cpp
+++ tx_60GHz.cpp
@@ -36,7 +36,8 @@ class board_60GHz_base {
   board_60GHz_base(uhd::usrp::dboard_iface::sptr db_iface,
 		      uhd::usrp::dboard_iface::unit_t unit,
 		 uint16_t enable_hmc, uint16_t data_in_hmc,
-      uint16_t clk_hmc, uint16_t data_out_hmc, uint16_t reset_hmc);
+  uint16_t clk_hmc, uint16_t data_out_hmc, uint16_t reset_hmc, 
+  uint16_t chip_addr);
 
   void write_row(uint16_t row_num, uint32_t value);
   uint16_t read_row(uint16_t row_num);
@@ -51,6 +52,7 @@ class board_60GHz_base {
   uint16_t m_enable_hmc, m_data_in_hmc, m_data_out_hmc;
   uint16_t m_clk_hmc, m_reset_hmc, m_dummy;
   uint16_t mask_read, mask_write, mask_all;
+  uint16_t m_chip_addr;
 
   uhd::usrp::dboard_iface::sptr m_db_iface;
   uhd::usrp::dboard_iface::unit_t m_unit;
@@ -89,10 +91,10 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
         ("args", po::value<std::string>(&args)->default_value(""), "simple uhd device address args")
         ("secs", po::value<double>(&seconds_in_future)->default_value(3), "number of seconds in the future to transmit")
         ("nsamps", po::value<size_t>(&total_num_samps)->default_value(1000), "total number of samples to transmit")
-        ("txrate", po::value<double>(&tx_rate)->default_value(100e6/16), "rate of outgoing samples")
-        ("freq", po::value<double>(&freq)->default_value(0), "rf center frequency in Hz")
+        ("txrate", po::value<double>(&tx_rate)->default_value(100e6/4), "rate of outgoing samples")
+        ("freq", po::value<double>(&freq)->default_value(70e6), "rf center frequency in Hz")
         ("LOoffset", po::value<double>(&LOoffset)->default_value(0), "Offset between main LO and center frequency")
-        ("forever",po::value<bool>(&forever)->default_value(false), "run indefinetly")
+        ("forever",po::value<bool>(&forever)->default_value(true), "run indefinetly")
         ("10MHz",po::value<bool>(&use_external_10MHz)->default_value(false), 
 	     "external 10MHz on 'REF CLOCK' connector (true=1=yes)")
       //("PPS",po::value<bool>(&trigger_with_pps)->default_value(false), 
@@ -160,11 +162,12 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     int enable_hmc=3;
     int reset_hmc=4;
 
-
+    //int chip_addr=2+4; // TX
+    int chip_addr=1+2+4; // RX
 
     board_60GHz_base my_60GHz_board(db_iface,uhd::usrp::dboard_iface::UNIT_TX,
 		 enable_hmc, data_in_hmc,
-		 clk_hmc, data_out_hmc, reset_hmc);
+				    clk_hmc, data_out_hmc, reset_hmc,chip_addr);
 
 
     //my_60GHz_board.write_row(0,0); // Power everything on
@@ -173,33 +176,123 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     //my_60GHz_board.write_row(1,0); // Highest Q and everything on
     //my_60GHz_board.write_row(2,0xF0); // Normal operation
 
+    #if 0
     usleep(1e6);
-    my_60GHz_board.write_row(3,3) ;
+    my_60GHz_board.write_row(3,4) ;
     usleep(1e6);    
     std::cout << "Read=" << my_60GHz_board.read_row(3) << "\n"; 
     exit(1);
+    #endif
 
-    my_60GHz_board.write_row(3,0x1F); // Normal operation
-    my_60GHz_board.write_row(4,0x3F); // Normal operation
-    my_60GHz_board.write_row(5,0x04); // Normal operation
-    my_60GHz_board.write_row(6,0x8F); // Normal operation
+    #if 0 // TX
 
-    my_60GHz_board.write_row(7,0x0F); // Highest gain + normal operation
-    // my_60GHz_board.write_row(7,0xDF); // Lowest gain + normal operation
-    my_60GHz_board.write_row(8,0xBF); // normal operation
-    my_60GHz_board.write_row(9,0x6F); // normal operation
-    my_60GHz_board.write_row(10,0xF0); // normal operation @ 285.7143MHz ref.
-    my_60GHz_board.write_row(11,0x77); // 60GHz carrier frequency 0111 011 0
-    my_60GHz_board.write_row(12,0x5F); // Syntesizer parameters
-    my_60GHz_board.write_row(13,0x80); // normal operation (synths on)
-    my_60GHz_board.write_row(14,0x76); // normal operation
+    my_60GHz_board.write_row(0,0); // Power on everything
+    my_60GHz_board.write_row(1,0); // Power on and highest Q of filter.
+    my_60GHz_board.write_row(2,240); // Taken from PC app.
+    my_60GHz_board.write_row(3,31); // Taken from PC app.
+    my_60GHz_board.write_row(4,63); // Normal operation
+    my_60GHz_board.write_row(5,244); // Normal operation
+    my_60GHz_board.write_row(6,143); // 
+
+    int l_tx_gain=13; // 0:13. Increasing gain.
+    my_60GHz_board.write_row(7,15+16*(13-l_tx_gain)); 
+                                   // Highest gain + normal operation
     
+    my_60GHz_board.write_row(8,191); // normal operation
+    my_60GHz_board.write_row(9,111); // normal operation
 
-    for (int i1=0;i1<1000;i1++) {
-      std::cout << "Lock state=" << (my_60GHz_board.read_row(15) >>4) 
+
+    // Table 10. 285.7143MHz Reference
+    // Frequency(GHz)    DIVRATIO            BAND
+
+    /*      57             00001             000
+            57.5           00010             000
+            58             00011             001
+            58.5           00100             001
+            59             00101             010
+            59.5           00110             010
+            60             00111             011
+            60.5           01000             011
+            61             01001             100
+            61.5           01010             100
+            62             01011             101
+            62.5           01100             101
+            63             01101             110
+            63.5           01110             110
+            64             01111             111  */
+
+
+    my_60GHz_board.write_row(10,240); // 240+DIVRATIO<4>
+    my_60GHz_board.write_row(11,16*(1+2+4)+2*3+1); // 16*DIVRATIO<3:0>+2*BAND+1
+
+
+    my_60GHz_board.write_row(12,95); // Syntesizer parameters (lock window)
+    my_60GHz_board.write_row(13,128); // normal operation (synths on)
+    my_60GHz_board.write_row(14,118); // normal operation
+
+
+    #endif 
+
+
+    #if 1 // RX
+
+    my_60GHz_board.write_row(0,128); // Everthing on except ASK mod.
+    int bb_gain1=1; // 0-3
+    int bb_gain2=1; // 0-3
+    int bb_att1=3-bb_gain1;
+    int bb_att2=3-bb_gain2;
+    my_60GHz_board.write_row(1,bb_att2+4*bb_att1); // Power on + gain control
+
+    int bb_gain_fineI=5; // 0-5
+    int bb_gain_fineQ=5; // 0-5
+
+    int bb_att_fineI=5-bb_gain_fineI;
+    int bb_att_fineQ=5-bb_gain_fineQ;
+
+
+    my_60GHz_board.write_row(2,4*bb_att_fineQ+32*bb_att_fineI); 
+                                               // Normal operation
+
+    int bb_low_pass_corner=0; // 0=>1.4GHz, 1=>500MHz, 2=> 300MHz, 3=>200MHz.
+    int bb_high_pass_corner=0; // 0=>30kHz, 1=>300kHz, 2=>1.5MHz.
+
+    my_60GHz_board.write_row(3,3+16*bb_high_pass_corner+64*bb_low_pass_corner);
+                                            // Normal operation
+    
+    my_60GHz_board.write_row(4,158); // Normal operation
+
+    int if_gain=15; // 0-15
+    int if_att=15-if_gain;
+
+    my_60GHz_board.write_row(5,15+16*if_att); // Normal operation
+    my_60GHz_board.write_row(6,191); // Normal operation
+    my_60GHz_board.write_row(7,109); // Normal operation
+    my_60GHz_board.write_row(8,128); // Normal operation
+    my_60GHz_board.write_row(9,0); // Normal operation
+    my_60GHz_board.write_row(10,240); // 240+DIVRATIO<4>
+    my_60GHz_board.write_row(11,16*(1+2+4)+2*3+1); // 16*DIVRATIO<3:0>+2*BAND+1
+    my_60GHz_board.write_row(12,95); // Normal operation
+    my_60GHz_board.write_row(13,128); // Normal operation
+    my_60GHz_board.write_row(14,118); // Normal operation
+
+    #endif 
+
+    for (int i1=0;i1<15;i1++) {
+      std::cout << "reg=" << i1 << " value=" << my_60GHz_board.read_row(i1) 
+          << "\n";
+    };
+
+    
+    #if 1
+    for (int i1=0;i1<10;i1++) {
+      std::cout << "Lock state=" << (my_60GHz_board.read_row(15)>> 6 ) 
 		<< " (01=lock)" << "\n";
       usleep(1e6);
     };
+    #endif
+
+
+
 
     //dev->set_tx_bandwidth(36e6);
 
@@ -213,7 +306,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     tr=dev->set_tx_freq(trq,0);
     
 
-    dev->set_tx_gain(gain);
+    //dev->set_tx_gain(gain);
     std::cout << tr.to_pp_string() << "\n";
  
 
@@ -231,8 +324,6 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     dev->set_tx_rate(tx_rate);
     std::cout << boost::format("Actual TX Rate: %f Msps...") % (dev->get_tx_rate()/1e6) << std::endl;
     std::cout << boost::format("Setting device timestamp to 0...") << std::endl;
-
-
 
 
     
@@ -311,7 +402,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 board_60GHz_base::board_60GHz_base(uhd::usrp::dboard_iface::sptr db_iface,
 		      uhd::usrp::dboard_iface::unit_t unit,
 		 uint16_t enable_hmc, uint16_t data_in_hmc,
-	 uint16_t clk_hmc, uint16_t data_out_hmc, uint16_t reset_hmc)
+				   uint16_t clk_hmc, uint16_t data_out_hmc, uint16_t reset_hmc, uint16_t chip_addr )
 {
 
 
@@ -347,6 +438,7 @@ board_60GHz_base::board_60GHz_base(uhd::usrp::dboard_iface::sptr db_iface,
   m_clk_hmc=clk_hmc;
   m_data_out_hmc=data_out_hmc;
   m_reset_hmc=reset_hmc;
+  m_chip_addr=chip_addr;
 
   mask_write=(1<<m_enable_hmc) | (1<<m_data_out_hmc) | (1<<m_clk_hmc) | 
     (m_reset_hmc);
@@ -362,6 +454,19 @@ board_60GHz_base::board_60GHz_base(uhd::usrp::dboard_iface::sptr db_iface,
   m_db_iface->set_gpio_out(m_unit,1 << m_enable_hmc,1 << m_enable_hmc ); 
   m_db_iface->set_gpio_out(m_unit,0,1 << m_clk_hmc); 
 
+  /*
+  while (1) {
+
+  m_db_iface->set_gpio_out(m_unit,1 << m_enable_hmc,1 << m_enable_hmc ); 
+  usleep(1e3);
+  m_db_iface->set_gpio_out(m_unit,0,1 << m_enable_hmc ); 
+  usleep(1e3);
+
+
+  };
+  */
+
+
   // Let reset go high
   m_db_iface->set_gpio_out(m_unit,1<< m_reset_hmc,1 << m_reset_hmc); 
   usleep(2e5);
@@ -375,7 +480,7 @@ void board_60GHz_base::write_row(uint16_t row_num, uint32_t value) {
   int16_t data;
   double T=1000; // Use to increase clock period
   
-  uint32_t value_brs; // Bit reversed value
+  //uint32_t value_brs; // Bit reversed value
   uint32_t row_num_brs; // Bit reversed row number
 
   row_num=row_num & 0xF;
@@ -385,12 +490,14 @@ void board_60GHz_base::write_row(uint16_t row_num, uint32_t value) {
   for (int i1=0;i1<6;i1++)
     row_num_brs=row_num_brs | (((row_num>>i1) & 1) << (5-i1));
 
-  value_brs=0;
-  for (int i1=0;i1<8;i1++)
-    value_brs=value_brs | (((value >>i1) & 1) << (7-i1));
+  //value_brs=0;
+  //for (int i1=0;i1<8;i1++)
+  //  value_brs=value_brs | (((value >>i1) & 1) << (7-i1));
 
-  total_write=(1<<14) | (0<<15) | (1<<16) | (1<<17); // R/W +chip address
+  //total_write=(1<<14) | (0<<15) | (1<<16) | (1<<17); // R/W +chip address
   //total_write=(1<<14) | (1<<15) | (1<<16) | (1<<17); // R/W +chip address
+  total_write=(1<<14) | (m_chip_addr<<15);
+
   total_write=total_write | value;
   total_write=total_write | (row_num << 8);
 
@@ -440,7 +547,7 @@ uint16_t board_60GHz_base::read_row(uint16_t row_num) {
   int16_t data;
   double T=1000; // Use to increase clock period
   
-  uint32_t value_brs; // Bit reversed value
+  //uint32_t value_brs; // Bit reversed value
   uint32_t row_num_brs; // Bit reversed row number
 
 
@@ -454,13 +561,14 @@ uint16_t board_60GHz_base::read_row(uint16_t row_num) {
   };
 
   
-  value_brs=0;
-  for (int i1=0;i1<8;i1++)
-    value_brs=value_brs | (((value >>i1) & 1) << (7-i1));
+  //value_brs=0;
+  //for (int i1=0;i1<8;i1++)
+  //  value_brs=value_brs | (((value >>i1) & 1) << (7-i1));
 
 
-  total_write=(0<<14) | (0<<15) | (1<<16) | (1<<17); // R/W +chip address
+  //total_write=(0<<14) | (0<<15) | (1<<16) | (1<<17); // R/W +chip address
   //total_write=(0<<14) | (1<<15) | (1<<16) | (1<<17); // R/W +chip address
+  total_write=(0<<14) | (m_chip_addr<<15);
   total_write=total_write | value;
   total_write=total_write | (row_num << 8);
 
