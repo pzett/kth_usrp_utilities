b7758699cd168e620ae3217b87f2c20208c75878
EQ2430 <eq2430@eq2430-computer5.(none)>
2014-10-31 15:20:28 +0100

    .

--- demo_MIMO_rx.m
+++ demo_MIMO_rx.m
@@ -13,28 +13,30 @@ end;
  
 rf_freq=60e9;
 gain_rx=14;
+
 rand('twister',0);
-bits_in=rand(1,118)>0.5;
-%load bits_in
-[waveform, parameters]=modem_OFDM4(60,4,1,[1,2],1,bits_in,[],10,2,1:3);
+bits_in1=rand(1,1740)>0.5;
+bits_in2=rand(1,1740)>0.5;
+Ns=60;
+[waveform1, parameters1]=modem_OFDM4(Ns,4,[1,2],[1,2],1,bits_in1,[1,2,5:(Ns+2)]);
+[waveform2, parameters2]=modem_OFDM4(Ns,4,[3,4],[2,1],2,bits_in2,[3:(Ns+2)]);
 
+X=rx_60GHz_MIMO(10000,gain_rx,rate,scaling_8bits)
+[ start_pos, f_offset] = synchronize_OFDM1(X(1,1:5000), parameters1,1,1, 1);
 
-X=rx_60GHz(rf_freq,10000,0,gain_rx,rate,scaling_8bits);
-
-[ start_pos, f_offset] = synchronize_OFDM1(X(1:5000), parameters,1,1, 1);
-
-X=X.*exp(-j*2*pi*f_offset*(1:length(X)));
+X(1,:)=X(1,:).*exp(-j*2*pi*f_offset*(1:length(X)));
+X(2,:)=X(2,:).*exp(-j*2*pi*f_offset*(1:length(X)));
 
 while (1)
 
-  [hard_bits,h,rx,power,CPECS] = demod_OFDM4(X, parameters,start_pos+144);
+  [hard_bits,h,rx,power,CPECS] = demod_OFDM4(X(1,:), parameters1,start_pos+144);
 
   figure(1);
   plot(rx(:),'x');
-  BER=1-mean(hard_bits'==bits_in);
+  BER=1-mean(hard_bits'==bits_in1);
   text(-1.8,1.8,['BER=',num2str(round(BER*1000)/10),'%'],'FontSize',48);
   axis([-2 2 -2 2]);
-  e=evm(rx(:),parameters.tx(:));
+  e=evm(rx(:),parameters1.tx(:));
   text(1.0,1.8,['EVM=',num2str(round(e*10)/10),'%'],'FontSize',48);
   title('60GHz transmission');
   figure(2);  
