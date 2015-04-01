4315d52aad7eef083ee909f4eea92c0b7fe5f10a
pzett <per.zetterberg@ee.kth.se>
2015-04-01 17:37:59 +0200

    changed back

--- demo_tx.m
+++ demo_tx.m
@@ -1,3 +1,5 @@
+
+
 %===================================================
 % Set these parameters to obtain desired behaviour 
 %===================================================
@@ -5,7 +7,6 @@ usrp_addr='192.168.20.2';
 use_50Msps=0;
 gain_tx=13;
 rf_freq=60e9;
-rf_freq=15e6;
 const_size=4; %% 4=QPSK, 16=16QAM, 64=64QAM
 %===================================================
 
@@ -24,9 +25,5 @@ else
 end;
 
 
-%tx_60GHz(rf_freq, 3000, waveform*3000/sqrt(parameters.power),0, gain_tx, ...
-%rate, low_res, usrp_addr);
-
-tx(3000,rf_freq, waveform*3000/sqrt(parameters.power) , 0, ...
-   gain_tx+10,  rate, low_res);
-
+tx_60GHz(rf_freq, 3000, waveform*3000/sqrt(parameters.power),0, gain_tx, ...
+rate, low_res, usrp_addr);
